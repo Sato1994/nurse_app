4f52be653e13cb5b4305fdc1c0392bf124a61df0
IRIE S <hiyunnrti@gmail.com>
2021-04-28 18:45:56 +0900

    user新規登録ページの作成

--- front/src/main.js
+++ front/src/main.js
@@ -2,10 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import Router from './router/index.js'
-import axios from 'axios'
 
 const app = createApp(App).use(Router)
-// グローバルにthis.axiosで呼び出せるようになる。
-app.config.globalProperties.axios = axios
 
 app.mount('#app')
--- front/src/views/Welcom.vue
+++ front/src/views/Welcom.vue
@@ -1,4 +1,5 @@
 <template>
+:name, :email, :address, :wanted, :sex, :age, :year
 <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto flex flex-wrap items-center">
     <div class="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
@@ -15,8 +16,24 @@
         <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
         <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
       </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録してみる</button>
-      <p class="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+      <div class="relative mb-4">
+        <label for="age" class="leading-7 text-sm text-gray-600">年齢</label>
+        <input type="text" id="age" name="age" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="year" class="leading-7 text-sm text-gray-600">経験年数</label>
+        <input type="text" id="year" name="year" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="sex" class="leading-7 text-sm text-gray-600">性別</label>
+        <input type="text" id="sex" name="sex" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+      </div>
+      <div class="relative mb-4">
+        <label for="wanted" class="leading-7 text-sm text-gray-600">登録と同時に求職を開始しますか？</label>
+        <input type="checkbox" id="wanted" name="wanted" class="form-checkbox rounded text-pink-500" />
+      </div>
+      <button v-on:click="createNewUser" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録してみる</button>
+      <p class="text-xs text-gray-500 mt-3">求職を開始にチェックすると条件の良い病院からスカウトがくるかもしれません♪</p>
     </div>
   </div>
 </section>
@@ -24,6 +41,27 @@
 
 
 <script>
-
+import axios from 'axios'
+// export default でメンバーを囲むことで、外部からも参照できるようにする。
+export default {
+  setup() {
+    function createNewUser() {
+      axios.post('http://localhost:3000/api/user', {
+        name: "mochichann",
+        email: "mocanntijijy@mochichan.com",
+        password: "password"
+      })
+      .then(function (response) {
+        console.log(response)
+        })
+      .catch(function (error) {
+         console.log(error)
+      })
+    }
+    return {
+      createNewUser
+    }
+  }
+}
 
 </script>
