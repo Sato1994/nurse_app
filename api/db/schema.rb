b1e4a08bbc067a09d32bc33562a1db069456a17c
IRIE S <hiyunnrti@gmail.com>
2021-07-11 10:44:41 +0900

    FreeTimeモデル作成 モデルテスト作成 #44

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_214014) do
+ActiveRecord::Schema.define(version: 2021_07_10_124702) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2021_06_25_214014) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["host_id"], name: "index_agreements_on_host_id"
     t.index ["user_id"], name: "index_agreements_on_user_id"
+  end
+
+  create_table "free_times", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "start_time", null: false
+    t.datetime "finish_time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_free_times_on_user_id"
   end
 
   create_table "host_skills", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -146,6 +155,7 @@ ActiveRecord::Schema.define(version: 2021_06_25_214014) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "agreements", "hosts"
   add_foreign_key "agreements", "users"
+  add_foreign_key "free_times", "users"
   add_foreign_key "host_skills", "hosts"
   add_foreign_key "host_skills", "skills"
   add_foreign_key "user_skills", "skills"
