ceddd4554576364c2b480510b3f96761bbec5beb
IRIE S <hiyunnrti@gmail.com>
2021-10-21 04:59:10 +0900

    offersを視認できるように実装wq

--- front/pages/host/_id/negotiations.vue
+++ front/pages/host/_id/negotiations.vue
@@ -1,4 +1,5 @@
 <template>
+<container>
 <v-list dense>
       <v-subheader>ログイン者のrequest一覧</v-subheader>
       <v-list-item-group
@@ -8,10 +9,33 @@
         <v-list-item>
           <v-list-item-content>
             <v-list-item-title v-text="request.formedRequest"></v-list-item-title>
+            <v-list-item-title v-text="request.user.name"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
+
+
+
+    <v-list dense>
+      <v-subheader>ログイン者のoffers一覧</v-subheader>
+      <v-list-item-group
+        v-for="offer in formedMyOffers"
+          :key="offer.formedOffer"
+          color="primary">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title v-text="offer.formedOffer"></v-list-item-title>
+            <v-list-item-title v-text="offer.user.name"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+
+
+
+</container>
+
 </template>
 
 
@@ -33,6 +57,15 @@ export default {
         return newObject
       })
       return requests
+    },
+    formedMyOffers() {
+      const offers = this.$store.getters['myInfo/getMyOffers'].map(obj=> {
+        const s = new Date(obj.start_time)
+        const f = new Date(obj.finish_time)
+        const newObject = {id: obj.id, user: obj.user, host: obj.host, formedOffer: `${s.getFullYear()}年${s.getMonth() +1}月${s.getDate()}日${s.getHours()}時${s.getMinutes()}分から${f.getFullYear()}年${f.getMonth() +1}月${f.getDate()}日${f.getHours()}時${f.getMinutes()}分`}
+        return newObject
+      })
+      return offers
     },
   },
 };
