d1ea8ca5a80768cb2dc9ab5fd2a0f05dd7d5393b
Sato1994 <hiyunnrti@icloud.com>
2021-11-19 01:39:57 +0900

    Roomへのカラム追加に伴いfront変更

--- front/pages/host/_id/negotiations.vue
+++ front/pages/host/_id/negotiations.vue
@@ -1,29 +1,40 @@
 <template>
-<container>
-<v-list dense>
+  <container>
+    <v-list dense>
       <v-subheader>ログイン者のrequest一覧</v-subheader>
       <v-list-item-group
         v-for="(request, index) in formedMyRequests"
-          :key="index"
-          color="primary">
+        :key="index"
+        color="primary"
+      >
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title v-text="request.formedRequest"></v-list-item-title>
+            <v-list-item-title
+              v-text="request.formedRequest"
+            ></v-list-item-title>
             <v-list-item-title v-text="request.user.name"></v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
 
-
-
     <v-list dense>
       <v-subheader>ログイン者のoffers一覧</v-subheader>
       <v-list-item-group
         v-for="(offer, index) in formedMyOffers"
-          :key="index"
-          color="primary">
-        <v-list-item @click="createRoom(offer.user.id, offer.formedOffer)">
+        :key="index"
+        color="primary"
+      >
+        <v-list-item
+          @click="
+            createRoom(
+              offer.id,
+              offer.user.id,
+              offer.start_time,
+              offer.finish_time
+            )
+          "
+        >
           <v-list-item-content>
             <v-list-item-title v-text="offer.formedOffer"></v-list-item-title>
             <v-list-item-title v-text="offer.user.name"></v-list-item-title>
@@ -31,56 +42,78 @@
         </v-list-item>
       </v-list-item-group>
     </v-list>
-
-
-
-</container>
-
+  </container>
 </template>
 
 
 <script>
 export default {
-  // data: () => ({
-  //   myRequests: '',
-
-  // }),
   // create() {
   //   this.myRequests = this.$store.getters['myInfo/getMyRequests']
   // },
+
   computed: {
     formedMyRequests() {
-      const requests = this.$store.getters['myInfo/getMyRequests'].map(obj=> {
-        const s = new Date(obj.start_time)
-        const f = new Date(obj.finish_time)
-        const newObject = {id: obj.id, user: obj.user, formedRequest: `${s.getFullYear()}年${s.getMonth() +1}月${s.getDate()}日${s.getHours()}時${s.getMinutes()}分から${f.getFullYear()}年${f.getMonth() +1}月${f.getDate()}日${f.getHours()}時${f.getMinutes()}分`}
-        return newObject
-      })
+      const requests = this.$store.getters['myInfo/getMyRequests'].map(
+        (obj) => {
+          const s = new Date(obj.start_time)
+          const f = new Date(obj.finish_time)
+          const newObject = {
+            id: obj.id,
+            user: obj.user,
+            formedRequest: `${s.getFullYear()}年${
+              s.getMonth() + 1
+            }月${s.getDate()}日${s.getHours()}時${s.getMinutes()}分から${f.getFullYear()}年${
+              f.getMonth() + 1
+            }月${f.getDate()}日${f.getHours()}時${f.getMinutes()}分`,
+          }
+          return newObject
+        }
+      )
       return requests
     },
     formedMyOffers() {
-      const offers = this.$store.getters['myInfo/getMyOffers'].map(obj=> {
+      const offers = this.$store.getters['myInfo/getMyOffers'].map((obj) => {
         const s = new Date(obj.start_time)
         const f = new Date(obj.finish_time)
-        const newObject = {id: obj.id, recruitmentTime: obj.recruitmentTime, user: obj.user, formedOffer: `${s.getFullYear()}年${s.getMonth() +1}月${s.getDate()}日${s.getHours()}時${s.getMinutes()}分から${f.getFullYear()}年${f.getMonth() +1}月${f.getDate()}日${f.getHours()}時${f.getMinutes()}分`}
+        const newObject = {
+          id: obj.id,
+          recruitmentTime: obj.recruitmentTime,
+          user: obj.user,
+          start_time: obj.start_time,
+          finish_time: obj.finish_time,
+          formedOffer: `${s.getFullYear()}年${
+            s.getMonth() + 1
+          }月${s.getDate()}日${s.getHours()}時${s.getMinutes()}分から${f.getFullYear()}年${
+            f.getMonth() + 1
+          }月${f.getDate()}日${f.getHours()}時${f.getMinutes()}分`,
+        }
         return newObject
       })
       return offers
     },
   },
   methods: {
-    createRoom(userID,formedOffer) {
-      this.$axios.post(`/api/rooms/user/${userID}`,{},{ headers: this.$cookies.get('authInfo') })
-      .then((response) => {
-        console.log(response.data)
-        // Room画面に移動しformedOfferの範囲で交渉してみよう！的なガイドをいれる
-        // offerを消す
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-    }
-  }
-};
-
+    createRoom(requestId, userID, startTime, finishTime) {
+      this.$axios
+        .post(
+          `/api/rooms/user/${userID}`,
+          {
+            start_time: startTime,
+            finish_time: finishTime,
+            request_id: requestId,
+          },
+          { headers: this.$cookies.get('authInfo') }
+        )
+        .then((response) => {
+          console.log(response.data)
+          // Room画面に移動しformedOfferの範囲で交渉してみよう！的なガイドをいれる
+          // offerを消す
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
 </script>
