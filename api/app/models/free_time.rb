749b8b26c1ecf379f2718dd432c97079b1bb7ec7
Sato1994 <hiyunnrti@icloud.com>
2021-11-19 02:02:19 +0900

    stateに限らずAgreementととの重複した時間はfree_timeに登録できない

--- api/app/models/free_time.rb
+++ api/app/models/free_time.rb
@@ -11,9 +11,6 @@ class FreeTime < ApplicationRecord
   validate :limitation_of_free_time
   validate :free_time_has_some_hours_grace
 
-
-  
-
   def duplication_of_free_time_for_same_user
    if FreeTime.where('finish_time >= ? && ? >= start_time && user_id = ?', start_time, finish_time, user_id).exists?
     errors.add(:start_time, "登録時間が重複しています。")
@@ -21,8 +18,8 @@ class FreeTime < ApplicationRecord
   end
 
   def duplication_of_agreement_for_same_user
-    if Agreement.where('finish_time >= ? && ? >= start_time && user_id = ? && state != ?', start_time, finish_time, user_id, 4).exists?
-      errors.add(:start_time, "登録時間が勤務予定時間と重複しています。")
+    if Agreement.where('finish_time >= ? && ? >= start_time && user_id = ?', start_time, finish_time, user_id).exists?
+      errors.add(:start_time, "一度契約した時間では登録できません。")
     end
   end
 
@@ -32,24 +29,9 @@ class FreeTime < ApplicationRecord
     end
   end
 
-  # 登録時間は現時刻から12時間後から可能(agreementnお現時刻との猶予を6時間以上に直す。)
   def free_time_has_some_hours_grace
     unless start_time > (Time.current + 12.hour)
       errors.add(:start_time, "登録時間は現在時刻より12時間以上時間の猶予が必要です。")
     end
   end
-
-  
-  
-
-
-
-
-
-
-
-
-
-
-
 end
