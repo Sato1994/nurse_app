fc0a320292868eee0e48df62ee941a1e362da41a
Sato1994 <hiyunnrti@icloud.com>
2021-11-21 11:40:07 +0900

    user_requestのバリデーションにAgreementとの重複チェックを作成

--- api/spec/models/user_request_spec.rb
+++ api/spec/models/user_request_spec.rb
@@ -29,11 +29,18 @@ RSpec.describe UserRequest, type: :model do
       expect(ur.errors[:start_time]).to include("病院の募集時間の範囲を超えています。")
     end
 
-    it "同じ時間帯のuser_requestがあれば失敗" do
+    it "同じ時間帯のuser_requesがあれば失敗" do
       user_request = create(:user_request)
       new_request = build(:user_request, user: user_request.user)
       new_request.valid?
       expect(new_request.errors[:start_time]).to include("同じ時間帯で申請済みです。")
+    end
+
+    it "同じ時間帯のAgreementがあれば失敗" do
+      agreement = create(:agreement)
+      user_request = build(:user_request, user: agreement.user)
+      user_request.valid?
+      expect(user_request.errors[:start_time]).to include("同じ時間帯で契約済みです。")
     end
 
     it "作成時点で開始時刻が7時間以上猶予があれば有効" do
