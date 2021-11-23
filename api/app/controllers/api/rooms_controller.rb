b0a8dcb4d87d5408fcde54ee3ec06d42b5adb585
Sato1994 <hiyunnrti@icloud.com>
2021-11-23 12:07:46 +0900

    roomのupdateアクション追加

--- api/app/controllers/api/rooms_controller.rb
+++ api/app/controllers/api/rooms_controller.rb
@@ -26,7 +26,6 @@ class Api::RoomsController < ApplicationController
 
   def create
     if api_user_signed_in?
-      @host = Host.find(params[:host_id])
       room = Room.new(room_user_signed_in_params)
       if room.save
         HostRequest.find(params[:request_id]).destroy
@@ -35,7 +34,7 @@ class Api::RoomsController < ApplicationController
         render json: room.errors, status: 400
       end
     elsif api_host_signed_in?
-      @user = User.find(params[:user_id])
+      user = User.find(params[:user_id])
       room = Room.new(room_host_signed_in_params)
       if room.save
         UserRequest.find(params[:request_id]).destroy
@@ -47,15 +46,83 @@ class Api::RoomsController < ApplicationController
       render body: nil, status: 401
     end
   end
-  
+
+  def update
+    room = Room.find(params[:id])
+    if api_user_signed_in? && current_api_user = room.user
+      if params[:start_time] 
+        if room.update(room_user_signed_in_params)
+          render json: room, status: 200
+        else
+          render json: room.errors, status: 400
+        end
+      else
+        case room.consensus
+        when "negotiating"
+          if room.update(consensus: "user")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        when "user"
+          if room.update(consensus: "negotiating")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        when "host"
+          if room.update(consensus: "conclusion")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        else
+          render body: nil, status: 400
+        end
+      end
+    elsif api_host_signed_in? && current_api_host = room.host
+      if params[:start_time] 
+        if room.update(room_host_signed_in_params)
+          render json: room, status: 200
+        else
+          render json: room.errors, status: 400
+        end
+      else
+        case room.consensus
+        when "negotiating"
+          if room.update(consensus: "host")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        when "user"
+          if room.update(consensus: "conclusion")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        when "host"
+          if room.update(consensus: "negotiating")
+            render json: room, status: 200
+          else
+            render json: room.errors, status: 400
+          end
+        else
+          render body: nil, status: 400
+        end
+      end
+    else
+      render body:nil, status: 403
+    end
+  end
+
   private
 
   def room_user_signed_in_params
-    params.permit(:room).merge(user: current_api_user, host: @host, start_time: Time.zone.parse(params[:start_time]), finish_time: Time.zone.parse(params[:finish_time]))
+    params.permit(:host_id).merge(user_id: current_api_user.id, start_time: Time.zone.parse(params[:start_time]), finish_time: Time.zone.parse(params[:finish_time]))
   end
 
   def room_host_signed_in_params
-    params.permit(:room).merge(user: @user, host: current_api_host, start_time: Time.zone.parse(params[:start_time]), finish_time: Time.zone.parse(params[:finish_time]))
+    params.permit(:user_id).merge(host_id: current_api_host.id, start_time: Time.zone.parse(params[:start_time]), finish_time: Time.zone.parse(params[:finish_time]))
   end
-
 end
--- api/app/models/room.rb
+++ api/app/models/room.rb
@@ -1,7 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
   belongs_to :host
-
   has_many :user_messages, dependent: :destroy
   has_many :host_messages, dependent: :destroy
 
@@ -10,7 +9,8 @@ class Room < ApplicationRecord
   validates :consensus, inclusion: { in: %w( negotiating user host conclusion cancelled) }, presence: :true
   validates :deletion, inclusion: { in: %w( na user host) }, presence: :true
 
-  validate :duplication_of_room
+  validate :duplication_of_room_create, on: :create
+  validate :duplication_of_room_update, on: :update
   validate :room_has_some_hours_grace
   validate :limitation_of_room_hours_max
   validate :limitation_of_room_hours_min
@@ -18,8 +18,14 @@ class Room < ApplicationRecord
   enum consensus: { negotiating: 0, user: 1, host: 2, conclusion:3, cancelled: 4 }, _suffix: true
   enum deletion: { na: 0, user: 1, host: 2}, _suffix: true
 
-  def duplication_of_room
+  def duplication_of_room_create
     if Room.where('finish_time >= ? && ? >= start_time && user_id = ? && host_id = ?', start_time, finish_time, user_id, host_id).exists?
+      errors.add(:start_time, "同じお相手と同じ時間帯で交渉中です。")
+    end
+  end
+
+  def duplication_of_room_update
+    if Room.where('finish_time >= ? && ? >= start_time && user_id = ? && host_id = ? && id != ?', start_time, finish_time, user_id, host_id, id).exists?
       errors.add(:start_time, "同じお相手と同じ時間帯で交渉中です。")
     end
   end
