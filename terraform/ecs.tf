e2e818e6b5fca73b43854f804db07fe74abe027e
Sato1994 <hiyunnrti@icloud.com>
2022-02-20 19:01:20 +0900

    [add] service discoveryでfargateコンテナ間通信完了 #117

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,22 +5,35 @@ resource "aws_ecs_cluster" "cluster" {
 #################################################################################
 
 # タスク定義#####################################################################
-resource "aws_ecs_task_definition" "nurse_container" {
-  family = "nurse_container"
+# front
+resource "aws_ecs_task_definition" "front_container" {
+  family = "front_container"
   cpu = "512"
   memory = "1024"
   network_mode = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  container_definitions = file("./definitions/nurse_container.json")
+  container_definitions = file("./definitions/front_container.json")
+  execution_role_arn = module.ecs_task_execution_role.iam_role_arn
+}
+
+# api
+resource "aws_ecs_task_definition" "api_container" {
+  family = "api_container"
+  cpu = "512"
+  memory = "1024"
+  network_mode = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = file("./definitions/api_container.json")
   execution_role_arn = module.ecs_task_execution_role.iam_role_arn
 }
 #################################################################################
 
 # ecsサービス####################################################################
-resource "aws_ecs_service" "nurse" {
-  name = "nurse"
+# front
+resource "aws_ecs_service" "front_container" {
+  name = "front_container"
   cluster = aws_ecs_cluster.cluster.arn
-  task_definition = aws_ecs_task_definition.nurse_container.arn
+  task_definition = aws_ecs_task_definition.front_container.arn
   desired_count = 1
   launch_type = "FARGATE"
   platform_version = "1.4.0"
@@ -30,7 +43,6 @@ resource "aws_ecs_service" "nurse" {
     assign_public_ip = false
     security_groups = [
       module.front_sg.security_group_id,
-      module.api_sg.security_group_id
     ]
 
     subnets = [
@@ -43,6 +55,36 @@ resource "aws_ecs_service" "nurse" {
     target_group_arn = aws_lb_target_group.front_container.arn
     container_name = "front_container"
     container_port = 8080
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+# api
+resource "aws_ecs_service" "api_container" {
+  name = "api_container"
+  cluster = aws_ecs_cluster.cluster.arn
+  task_definition = aws_ecs_task_definition.api_container.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  platform_version = "1.4.0"
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups = [
+      module.api_sg.security_group_id
+    ]
+
+    subnets = [
+      aws_subnet.private-1a.id,
+      aws_subnet.private-1c.id
+    ]
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.sd_api.arn
   }
 
   lifecycle {
---
+++ terraform/sd.tf
@@ -0,0 +1,27 @@
+# サービスディスカバリ ###########################################################
+resource "aws_service_discovery_private_dns_namespace" "nurse_internal" {
+  name = "nurse.internal.local"
+  vpc = aws_vpc.vpc.id
+}
+##################################################################################
+
+# サービス #######################################################################
+resource "aws_service_discovery_service" "sd_api" {
+  name = "sd_api"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.nurse_internal.id
+
+    dns_records {
+      ttl = 10
+      type = "A"
+    }
+    
+    routing_policy = "MULTIVALUE"
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+##################################################################################
