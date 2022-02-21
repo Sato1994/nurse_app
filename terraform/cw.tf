64ec083850c338e67b5f3ed8bc185e3526519739
Sato1994 <hiyunnrti@icloud.com>
2022-02-21 15:55:29 +0900

    [add] ecs rails db:migrate タスク追加 #117

--- terraform/cw.tf
+++ terraform/cw.tf
@@ -7,3 +7,8 @@ resource "aws_cloudwatch_log_group" "api" {
   name = "/ecs/api"
   retention_in_days = 180
 }
+
+resource "aws_cloudwatch_log_group" "api_db_migrate" {
+  name = "/ecs/api/db_migrate"
+  retention_in_days = 180
+}
