6a374e60a71c537840888754e6598449d22eba98
Sato1994 <hiyunnrti@icloud.com>
2022-03-05 17:32:53 +0900

    [fix] 自動テスト構築の際にコメントアウトしたテストをアンコメント #117

--- api/spec/models/recruitment_time_spec.rb
+++ api/spec/models/recruitment_time_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe RecruitmentTime, type: :model do
   end
 
   describe "start_time" do
-    # it "作成時点で開始時間まで12時間以上あれば有効" do
-    #   rt = build(:recruitment_time, start_time: Time.current + 12.hour + 1)
-    #   expect(rt).to be_valid
-    # end
+    it "作成時点で開始時間まで12時間以上あれば有効" do
+      rt = build(:recruitment_time, start_time: Time.current + 12.hour + 1)
+      expect(rt).to be_valid
+    end
 
     it "作成時点で開始時間までちょうど12時間前ならば無効" do
       rt = build(:recruitment_time, start_time: Time.current + 12.hour)
@@ -25,10 +25,10 @@ RSpec.describe RecruitmentTime, type: :model do
 
   describe "start_time, finish_time" do
     context "登録時間" do
-      # it "１時間なら有効" do
-      #   rt = build(:recruitment_time, finish_time: Time.current + 21.hour)
-      #   expect(rt).to be_valid
-      # end
+      it "１時間なら有効" do
+        rt = build(:recruitment_time, finish_time: Time.current + 21.hour)
+        expect(rt).to be_valid
+      end
 
       it "1時間未満なら無効" do
         rt = build(:recruitment_time, finish_time: Time.current + 21.hour - 1.second )
--- api/spec/requests/api/agreements_spec.rb
+++ api/spec/requests/api/agreements_spec.rb
@@ -7,29 +7,29 @@ RSpec.describe "Api::Agreements", type: :request do
   
   describe "GET /index" do
     context "stateが0のものは勤務時間内であればstate1へ変更される" do
-      # it "userがログインしている場合" do
-      #   create(:agreement, state: 0)
-      #   agreement = create(:agreement, state: 0)
-      #   user = agreement.user
-      #   create(:agreement, user: user, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
-      #   post "/api/user/sign_in", params: { email: user.email, password: user.password }
-      #   travel 24.hour - 1.second
-      #   get "/api/agreements", headers: headers
-      #   travel 1.second
-      #   expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 1).count}.from(0).to(1)
-      # end
+      it "userがログインしている場合" do
+        create(:agreement, state: 0)
+        agreement = create(:agreement, state: 0)
+        user = agreement.user
+        create(:agreement, user: user, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
+        post "/api/user/sign_in", params: { email: user.email, password: user.password }
+        travel 24.hour - 1.second
+        get "/api/agreements", headers: headers
+        travel 1.second
+        expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 1).count}.from(0).to(1)
+      end
 
-      # it "hostがログインしている場合" do
-      #   create(:agreement, state: 0)
-      #   agreement = create(:agreement, state: 0)
-      #   host = agreement.host
-      #   create(:agreement, host: host, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
-      #   post "/api/host/sign_in", params: { email: host.email, password: host.password }
-      #   travel 24.hour - 1.second
-      #   get "/api/agreements", headers: headers
-      #   travel 1.second
-      #   expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 1).count}.from(0).to(1)
-      # end
+      it "hostがログインしている場合" do
+        create(:agreement, state: 0)
+        agreement = create(:agreement, state: 0)
+        host = agreement.host
+        create(:agreement, host: host, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
+        post "/api/host/sign_in", params: { email: host.email, password: host.password }
+        travel 24.hour - 1.second
+        get "/api/agreements", headers: headers
+        travel 1.second
+        expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 1).count}.from(0).to(1)
+      end
     end
 
     context "atateが0か1のものは勤務終了時間後であればstate2へ変更される" do
@@ -39,25 +39,25 @@ RSpec.describe "Api::Agreements", type: :request do
       end
       let(:agreement) { create(:agreement, state: 0)}
       
-      # it "userがログインしている場合" do
-      #   user = agreement.user
-      #   create(:agreement, state: 1, user: user, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
-      #   post "/api/user/sign_in", params: { email: user.email, password: user.password }
-      #   travel 41.hour
-      #   get "/api/agreements", headers: headers
-      #   travel 1.second
-      #   expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 2).count}.from(1).to(2)
-      # end
+      it "userがログインしている場合" do
+        user = agreement.user
+        create(:agreement, state: 1, user: user, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
+        post "/api/user/sign_in", params: { email: user.email, password: user.password }
+        travel 41.hour
+        get "/api/agreements", headers: headers
+        travel 1.second
+        expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 2).count}.from(1).to(2)
+      end
 
-      # it "hostがログインしている場合" do
-      #   host = agreement.host
-      #   create(:agreement, state: 1, host: host, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
-      #   post "/api/host/sign_in", params: { email: host.email, password: host.password }
-      #   travel 41.hour
-      #   get "/api/agreements", headers: headers
-      #   travel 1.second
-      #   expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 2).count}.from(1).to(2)
-      # end
+      it "hostがログインしている場合" do
+        host = agreement.host
+        create(:agreement, state: 1, host: host, start_time: Time.current.change(usec: 0) + 33.hour, finish_time: Time.current.change(usec: 0) + 41.hour)
+        post "/api/host/sign_in", params: { email: host.email, password: host.password }
+        travel 41.hour
+        get "/api/agreements", headers: headers
+        travel 1.second
+        expect{get "/api/agreements", headers: headers}.to change{Agreement.where(state: 2).count}.from(1).to(2)
+      end
     end
     
     context "関与しているagreementの数だけjsonを返す" do
