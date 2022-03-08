7b6f30fecccb4172766efdc5b18aa4aa05aa31bc
Sato1994 <hiyunnrti@icloud.com>
2022-03-09 08:43:20 +0900

    [fix] conflict

--- api/config/puma.rb
+++ api/config/puma.rb
@@ -2,7 +2,6 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-<<<<<<< HEAD
 
 port        ENV.fetch("PORT") { 3000 }
 
@@ -11,33 +10,3 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 plugin :tmp_restart
-=======
-
-port        ENV.fetch("PORT") { 3000 }
-
-
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-plugin :tmp_restart
-
-
-
-########## puma用設定 ##########
-
-# workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
-
-# max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-# min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-# threads min_threads_count, max_threads_count
-
-# preload_app!
-
-# rackup      DefaultRackup
-# port        ENV.fetch("PORT") { 3000 }
-# environment ENV.fetch("RAILS_ENV") { "development" }
-
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection
-# end
->>>>>>> 74f8d2e759a314ef68c92dc85b5063d258608481
