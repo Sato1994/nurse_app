826af8e25b8f0712a106c0ee31619ade6f6b7a8e
Sato1994 <hiyunnrti@icloud.com>
2022-03-16 10:59:47 +0900

    [fix] cancell_comments カラム修正  #147

--- api/db/migrate/20220315040423_create_cancell_comments.rb
+++ api/db/migrate/20220315040423_create_cancell_comments.rb
@@ -1,8 +1,8 @@
 class CreateCancellComments < ActiveRecord::Migration[6.0]
   def change
     create_table :cancell_comments do |t|
-      t.references :agreement, null: false, foreign_key: true
-      t.text :comment
+      t.references :agreement, null: false, foreign_key: true, unique: true
+      t.text :comment, null: false
 
       t.timestamps
 
