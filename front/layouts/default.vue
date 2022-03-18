528a079253443710c470bb74556ab8460c565f7d
Sato1994 <hiyunnrti@icloud.com>
2022-03-18 16:39:20 +0900

    [add] snackbar作成   #152

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app id="inspire">
     <Header />
+
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
           <v-col>
             <v-sheet color="orange lighten-5" min-height="70vh" rounded="lg">
               <Nuxt />
+              <Snackbar />
             </v-sheet>
           </v-col>
         </v-row>
@@ -17,9 +19,12 @@
 
 <script>
 import Header from '@/components/Header.vue'
+import Snackbar from '@/components/Snackbar.vue'
+
 export default {
   components: {
     Header,
+    Snackbar,
   },
 
   data() {
