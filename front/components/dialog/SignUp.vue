0bedc45ecef53365d7f8a2a2abfb0595e205aac8
Sato1994 <hiyunnrti@icloud.com>
2022-03-20 01:44:08 +0900

    [update] フォームにフロント側のバリデーション追加 #160

--- front/components/dialog/SignUp.vue
+++ front/components/dialog/SignUp.vue
@@ -2,66 +2,109 @@
   <v-row justify="center">
     <v-dialog v-model="isDisplay" persistent max-width="600px">
       <v-card>
-        <v-card-title>
-          <span class="text-h5">新規登録</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  v-model="info.name"
-                  :counter="10"
-                  label="名前（フルネーム）"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field
-                  v-model="info.myid"
-                  label="サイト内ID"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col v-if="$cookies.get('user') === 'host'" cols="12">
-                <v-text-field
-                  v-model="info.address"
-                  label="住所"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="info.email"
-                  label="メールアドレス"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="info.password"
-                  label="パスワード"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="info.password_confirmation"
-                  label="パスワード確認用"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>全て入力必須です</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="warning darken-1" text @click="closeDialog">
-            閉じる
-          </v-btn>
-          <v-btn color="warning darken-1" text @click="signUp"> 登録 </v-btn>
-        </v-card-actions>
+        <ValidationObserver v-slot="{ invalid }">
+          <v-card-title>
+            <span class="text-h5">新規登録</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="6">
+                  <ValidationProvider
+                    v-slot="{ errors }"
+                    :rules="nameLengthRule"
+                    name="名前"
+                  >
+                    <v-text-field
+                      v-model="info.name"
+                      label="名前（フルネーム）"
+                      required
+                    ></v-text-field>
+                    <span class="red--text">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </v-col>
+                <v-col cols="12" sm="6" md="6">
+                  <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required|alpha_num|max:15"
+                    name="ID"
+                  >
+                    <v-text-field
+                      v-model="info.myid"
+                      label="サイト内ID"
+                      required
+                    ></v-text-field>
+                    <span class="red--text">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </v-col>
+                <v-col v-if="$cookies.get('user') === 'host'" cols="12">
+                  <v-text-field
+                    v-model="info.address"
+                    label="住所"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required|email"
+                    name="メールアドレス"
+                  >
+                    <v-text-field
+                      v-model="info.email"
+                      label="メールアドレス"
+                      required
+                    ></v-text-field>
+
+                    <span class="red--text">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </v-col>
+                <v-col cols="12">
+                  <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required|min:6"
+                    vid="confirmation"
+                    name="パスワード"
+                  >
+                    <v-text-field
+                      v-model="info.password"
+                      label="パスワード"
+                      required
+                    ></v-text-field>
+                    <span class="red--text">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </v-col>
+                <v-col cols="12">
+                  <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required|confirmed:confirmation"
+                    name="パスワード"
+                  >
+                    <v-text-field
+                      v-model="info.password_confirmation"
+                      label="パスワード確認用"
+                      required
+                    ></v-text-field>
+                    <span class="red--text">{{ errors[0] }}</span>
+                  </ValidationProvider>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="warning darken-1" text @click="closeDialog">
+              閉じる
+            </v-btn>
+            <v-btn
+              color="warning darken-1"
+              text
+              :disabled="invalid"
+              @click="signUp"
+            >
+              登録
+            </v-btn>
+          </v-card-actions>
+        </ValidationObserver>
       </v-card>
     </v-dialog>
   </v-row>
@@ -73,6 +116,14 @@ export default {
     isDisplay: false,
     info: {},
   }),
+
+  computed: {
+    nameLengthRule() {
+      return this.$cookies.get('user') === 'user'
+        ? 'required|max:20'
+        : 'required|max:40'
+    },
+  },
 
   methods: {
     signUp() {
@@ -121,6 +172,7 @@ export default {
     closeDialog() {
       this.$cookies.removeAll()
       this.isDisplay = false
+      this.info = ''
     },
   },
 }
--- front/plugins/vee-validate.js
+++ front/plugins/vee-validate.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import { ValidationProvider, extend, localize } from 'vee-validate'
-import { required, email } from 'vee-validate/dist/rules';
+import { ValidationProvider, ValidationObserver, extend, localize } from 'vee-validate'
+// inmportするルール
+import { between, max, required, email, min, alpha_num as alphaNum, confirmed } from 'vee-validate/dist/rules';
 import ja from 'vee-validate/dist/locale/ja.json';
 
 localize('ja', ja);
@@ -8,5 +9,12 @@ localize('ja', ja);
 // inmportするルール
 extend('required', required);
 extend('email', email);
+extend('min', min)
+extend('alpha_num', alphaNum)
+extend('confirmed', confirmed)
+extend('max', max)
+extend('between', between)
+
 
 Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver)
