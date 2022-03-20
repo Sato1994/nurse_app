16cab1d0768d132951c6e403ab48ad37bbac7812
Sato1994 <hiyunnrti@icloud.com>
2022-03-20 15:43:19 +0900

    [fix] たまに失敗するテストの修正 #161

--- api/spec/factories/agreements.rb
+++ api/spec/factories/agreements.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     association :user
     association :host
     association :room
-    start_time { 24.hours.from_now }
-    finish_time { 32.hours.from_now }
+    start_time { 24.hours.from_now.change(usec: 0) }
+    finish_time { 32.hours.from_now.change(usec: 0) }
     state { 0 }
   end
 end
