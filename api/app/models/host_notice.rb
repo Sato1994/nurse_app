a6e918f94758973e08294d79c1d3d49b376f5edd
Sato1994 <hiyunnrti@icloud.com>
2022-03-31 11:44:25 +0900

    [add] モデルテスト #199

--- api/app/models/host_notice.rb
+++ api/app/models/host_notice.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class HostNotice < ApplicationRecord
   belongs_to :source, polymorphic: true, optional: true
-
   belongs_to :host
+
+  # https://github.com/Sato1994/nurse_app/issues/199
+  validates :action, presence: true, inclusion: { in: %w[created left agreed changed cancelled] }
+  validates :checked, inclusion: { in: [true, false] }
+  validates :source, presence: true
 end
