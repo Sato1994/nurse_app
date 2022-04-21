b54824321fdaf4931727e49298f0700afc911c80
Sato1994 <hiyunnrti@icloud.com>
2022-04-21 17:28:54 +0900

    [update] agreementが作られていないrooms一覧のカード化 #232

---
+++ front/components/issues/Rooms.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="(room, i) in rooms"
+        :key="i"
+        cols="12"
+        sm="12"
+        md="12"
+        lg="6"
+      >
+        <TimeCard
+          :partnerLink="`/rooms/${room.id}`"
+          :cardIsHover="true"
+          :partnerName="room.partner.name"
+          :startTime="room.startTime"
+          :finishTime="room.finishTime"
+          color="warning"
+          :lockButton="false"
+          @click.native="$router.push(`/rooms/${room.id}`)"
+        >
+          <template #description>
+            <v-card-subtitle
+              v-if="room.state !== 'conclusion' || room.state !== 'cancelled'"
+            >
+              契約前
+            </v-card-subtitle>
+          </template>
+
+          >
+        </TimeCard>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import TimeCard from '@/components/TimeCard.vue'
+export default {
+  components: {
+    TimeCard,
+  },
+
+  computed: {
+    ...mapGetters('issues/rooms', ['rooms']),
+  },
+}
+</script>
