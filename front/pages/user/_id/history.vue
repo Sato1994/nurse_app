2f21905aa2052882a4d525206e723a4cec8b3b2d
Sato1994 <hiyunnrti@icloud.com>
2022-04-27 14:54:02 +0900

    [add] hisory page土台作成 #243

---
+++ front/pages/user/_id/history.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-card-title>山田孝之 </v-card-title>
+
+    <v-card-text>
+      <slot name="profile"></slot>
+      <v-btn
+        :to="$route.query.prev"
+        nuxt
+        class="mt-3"
+        rounded
+        small
+        depressed
+        block
+      >
+        プロフィール
+      </v-btn>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
