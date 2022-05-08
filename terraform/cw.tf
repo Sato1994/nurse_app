5f5497c476035f70586193a844bc207d00b792fc
IRIE <82074242+Sato1994@users.noreply.github.com>
2022-05-09 08:51:46 +0900

    Revert "Issue/273"

--- terraform/cw.tf
+++ terraform/cw.tf
@@ -12,8 +12,3 @@ resource "aws_cloudwatch_log_group" "api_db_migrate" {
   name = "/ecs/api/db_migrate"
   retention_in_days = 180
 }
-
-resource "aws_cloudwatch_log_group" "api_db_seed" {
-  name = "/ecs/api/db_seed"
-  retention_in_days = 180
-}
