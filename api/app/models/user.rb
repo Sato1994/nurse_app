0d927792e83bc1569fd6eb423a35eeb343f16449
Sato1994 <hiyunnrti@icloud.com>
2022-05-08 20:35:27 +0900

    [test] seed タスク定義追加 おためしpush #273

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -20,8 +20,8 @@ class User < ApplicationRecord
   has_many :user_notices, dependent: :destroy
 
   mount_uploader :image, ImageUploader
-  # geocoded_by :address, latitude: :lat, longitude: :lng
-  # after_validation :geocode
+  geocoded_by :address, latitude: :lat, longitude: :lng
+  after_validation :geocode
 
   def to_param
     myid
